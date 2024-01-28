69bdc99f8194f39b77d1f07a49bc4febee95d4b3
Константин Сергеев <xdsfbussines@gmail.com>
2024-01-28 22:33:33 +0300

    sdsdd

---
+++ index.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-unresolved */
+import readlineSync from 'readline-sync';
+
+const calculator = {
+  a: 0,
+  b: 0,
+  read: function() {
+    this.a = parseFloat(readlineSync.question('Введите первое число: '));
+    this.b = parseFloat(readlineSync.question('Введите второе число: '));
+  },
+  sum: function() {
+    return this.a + this.b;
+  },
+  mul: function() {
+    return this.a * this.b;
+  }
+};
+
+module.exports = calculator;
+
